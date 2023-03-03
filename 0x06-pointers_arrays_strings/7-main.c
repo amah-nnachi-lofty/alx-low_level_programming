5b5928a20e98f2b4fd015710dfc3a23b88740f7a
Amah-Nnachi-Lofty <delofty2k@yahoo.com>
2023-03-03 08:12:45 -0800

    Write a function that encodes a string into 1337.
        Letters a and A should be replaced by 4
        Letters e and E should be replaced by 3
        Letters o and O should be replaced by 0
        Letters t and T should be replaced by 7
        Letters l and L should be replaced by 1
        Prototype: char *leet(char *);
        You can only use one if in your code
        You can only use two loops in your code
        You are not allowed to use switch
        You are not allowed to use any ternary operation

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -4,8 +4,9 @@
 /**
  * main - check the code for
  *
- * Return: Always 0.
+ * Return: Always 0
  */
+
 int main(void)
 {
     char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
