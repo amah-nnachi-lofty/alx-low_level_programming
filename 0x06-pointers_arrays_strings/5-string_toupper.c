d9eb5029b3272731e9426ce68fdb1472d51e62fd
Amah-Nnachi-Lofty <delofty2k@yahoo.com>
2023-03-03 07:49:52 -0800

    Write a function that changes all lowercase letters of a string to uppercase.
        Prototype: char *string_toupper(char *);

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters
+ *                  of a string to uppercase.
+ * @str: The string to be changed.
+ *
+ * Return: A pointer to the changed string.
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		n[i] = (int)n[i] - 32;
+	}
+	return (n);
+}
