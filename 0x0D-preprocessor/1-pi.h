4247cea9c26a19fd5eb87e2a46081ca9795197de
Amah-Nnachi-Lofty <delofty2k@yahoo.com>
2023-03-20 12:37:00 -0700

    Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+#ifndef _PI_H_
+#define _PI_H_
+
+#define PI 3.14159265359
+/*
+ * File: 1-pi.h
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#endif /* _PI_H_ */
