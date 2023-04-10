447d9051a754976e5a3df611196f69c7918ba58b
Amah-Nnachi-Lofty <delofty2k@yahoo.com>
2023-04-10 12:45:38 -0700

    Write a function that sets the value of a bit to 0 at a given index.
        Prototype: int clear_bit(unsigned long int *n, unsigned int index);
        where index is the index, starting from 0 of the bit you want to set
        Returns: 1 if it worked, or -1 if an error occurred

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0 at a specified index. 
- * * @n: a pointer to a decimal number that needs to be changed as an argument.
- * @index: The index position to change starts at a given value.
- * Return: returns a value of 1. However, if an error occurs, it returns -1.
+ * clear_bit - sets the value of a bit to 0.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
