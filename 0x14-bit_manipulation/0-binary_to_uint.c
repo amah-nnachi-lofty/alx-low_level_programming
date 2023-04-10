13df5a42c1347e6c12577f1baa5966cf7957a3bb
Amah-Nnachi-Lofty <delofty2k@yahoo.com>
2023-04-10 07:29:36 -0700

    Write a function that converts a binary number to an unsigned int.
        Prototype: unsigned int binary_to_uint(const char *b);
        where b is pointing to a string of 0 and 1 chars
        Return: the converted number, or 0 if
            there is one or more chars in the string b that is not 0 or 1
            b is NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 /**
- * binary_to_uint: to convert a binary number to an unsigned int
+ * binary_to_uint will convert a binary number to an unsigned int
  * @b: a binary pointer to a string chars of 0 and 1
- * Return: the converted number, or 0 if b is NULL, there is one 
+ *
+ * Return: the converted number, or 0 if b is NULL, there is one
+ *
  * or more chars in the string b that is not 0 or 1
  */
 unsigned int binary_to_uint(const char *b)
