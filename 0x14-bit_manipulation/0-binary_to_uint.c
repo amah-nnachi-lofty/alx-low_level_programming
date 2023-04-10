903504e372322aba4dd3c8f816b281f2a8fb61e2
Amah-Nnachi-Lofty <delofty2k@yahoo.com>
2023-04-10 06:51:37 -0700

    Write a function that converts a binary number to an unsigned int.
        Prototype: unsigned int binary_to_uint(const char *b);
        where b is pointing to a string of 0 and 1 chars
        Return: the converted number, or 0 if
            there is one or more chars in the string b that is not 0 or 1
            b is NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,30 +3,28 @@
 
 /**
  * binary_to_uint: to convert a binary number to an unsigned int
- * 
  * @b: a binary pointer to a string chars of 0 and 1
- * 
- * Return: the converted number, or 0 if b is NULL, there is one or more chars in the string b that is not 0 or 1
- *
+ * Return: the converted number, or 0 if b is NULL, there is one 
+ * or more chars in the string b that is not 0 or 1
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int name1, raise_power;
+unsigned int n1, p2; 
 int int_len;
-	if (b == NULL)
-		return (0);
+    if (b == NULL)
+        return (0);
 
-	    for (int_len = 0; b[int_len]; int_len++)
-	    {
-	if (b[int_len] != '0' && b[int_len] != '1')
-			return (0);
-	    }
+    for (int_len = 0; b[int_len]; int_len++)
+    {
+    if (b[int_len] != '0' && b[int_len] != '1')
+        return (0);
+    }
 
-	    for (raise_power = 1, name1 = 0, int_len--; int_len >= 0; int_len--, raise_power *= 2)
-	    {
-	if (b[int_len] == '1')
-			name1 += raise_power;
-	    }
+    for (p2 = 1, n1 = 0, int_len--; int_len >= 0; int_len--, p2 *= 2)
+    {
+    if (b[int_len] == '1')
+        n1 += p2;
+    }
 
-	return (name1);
+    return (n1);
 }
