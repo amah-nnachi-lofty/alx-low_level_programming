f9d2e2a02a96802511d3e9e74c05ab0978f9c30d
Amah-Nnachi-Lofty <delofty2k@yahoo.com>
2023-04-10 17:43:26 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,27 +9,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int f;
+	int fname;
+	int rwrite;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (f == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (fname = 0; text_content[fname]; fname++)
 		;
 
-	rwr = write(fd, text_content, nletters);
+	fname = write(f, text_content, fname);
 
-	if (rwr == -1)
+	if (fname == -1)
 		return (-1);
 
 	close(fd);
