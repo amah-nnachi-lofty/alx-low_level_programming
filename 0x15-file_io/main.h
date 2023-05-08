8301c3100c8afea2f8b64932b03bfdcd950737a7
Amah Nachi Lofty <loftdot@gmail.com>
2023-05-08 10:59:06 -0700

    Write a program that copies the content of a file to another file.
        Usage: cp file_from file_to
        if the number of argument is not the correct one, exit with code 97 and print Usage: cp file_from file_to, followed by a new line, on the POSIX standard error.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@
 #include <stdio.h>
 
 /*Prototypes & functions*/
+void close_file(int file_descriptor);
 char *create_buffer(char *file_name);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
